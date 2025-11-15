f3b4f75afc476230e3084da7853c00d5eeced9af
Víh Albuquerque <ti.vih498@outlook.com>
2025-11-14 22:42:25 -0300

    Add files via upload

---
+++ lang/pt_br/local_studentanalytics.php
@@ -0,0 +1,18 @@
+<?php
+// Arquivo de idioma para o plugin Moodle Student Analytics.
+// Local: /local/studentanalytics/lang/pt_br/local_studentanalytics.php
+
+$string['pluginname'] = 'Análise de Estudantes';
+$string['title'] = 'Painel de Análise dos Estudantes';
+$string['welcome'] = 'Bem-vindo ao painel de análise de dados dos estudantes!';
+$string['top10'] = '👥 Top 10 alunos mais ativos';
+$string['no_data'] = 'Nenhum dado encontrado.';
+$string['error'] = 'Erro ao carregar os dados.';
+$string['totalaccess'] = 'acessos';
+$string['topstudents'] = 'Top 10 alunos mais ativos';
+$string['averageaccess'] = 'Tempo médio de acesso';
+$string['forumparticipation'] = 'Participação em fóruns';
+$string['submissions'] = 'Entregas de atividades';
+$string['correlation'] = 'Correlação Engajamento x Notas';
+$string['exportcsv'] = 'Exportar CSV';
+$string['lowalerts'] = 'Alunos em risco';
